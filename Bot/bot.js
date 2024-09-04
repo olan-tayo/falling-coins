007267164d3c654c8a0780e783b1185357f34e2c
olan-tayo <olantayo3627@gmail.com>
2024-09-04 17:50:48 +0100

    chore: add button to bot

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,12 +2,12 @@ const { Telegraf } = require("telegraf");
 const bot = new Telegraf("7538734070:AAHyqf1ejhzLwvIZe7QcLdRDRK4mNLoNuRg");
 
 console.log(bot);
-// bot.start((ctx) => {
-//   ctx.reply("t.me/fallingcoins_bot/flyingcoins.");
-// });
+bot.start((ctx) => {
+  ctx.reply("t.me/fallingcoins_bot/flyingcoins.");
+});
 
-// bot.help((ctx) => {
-//   ctx.reply("Available commands:\n/newarticle - Create a new article");
-// });
+bot.help((ctx) => {
+  ctx.reply("Available commands:\n/newarticle - Create a new article");
+});
 
 bot.launch();
