dbb16d630a0411c1b194ce614f8ae05659909c2f
olan-tayo <olantayo3627@gmail.com>
2024-09-04 14:38:04 +0100

    feat: add telegram bot

--- bot/bot.js
+++ bot/bot.js
@@ -2,10 +2,6 @@ const { Telegraf } = require("telegraf");
 const bot = new Telegraf("7538734070:AAHyqf1ejhzLwvIZe7QcLdRDRK4mNLoNuRg");
 
 bot.start((ctx) => {
-  ctx.reply("Welcome to Falling Coins :)");
-});
-bot.command("start", (ctx) => {
-  // Create an inline keyboard with a button linking to a URL
   ctx.reply(
     "Welcome! Click the button below:",
     Markup.inlineKeyboard([
