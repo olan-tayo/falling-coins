ba7fb459512b01bb330a43a8f818506f43433645
olan-tayo <olantayo3627@gmail.com>
2024-09-04 17:03:55 +0100

    refactor: refactor bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+const { Telegraf } = require("telegraf");
+const bot = new Telegraf("7538734070:AAHyqf1ejhzLwvIZe7QcLdRDRK4mNLoNuRg");
+
+bot.start((ctx) => {
+  ctx.reply("t.me/fallingcoins_bot/flyingcoins.");
+});
+
+bot.help((ctx) => {
+  ctx.reply("Available commands:\n/newarticle - Create a new article");
+});
+
+bot.launch();
