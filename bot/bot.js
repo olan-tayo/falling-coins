5a0496698c53ac7194293ceedf7ef18d5918941d
olan-tayo <olantayo3627@gmail.com>
2024-09-04 15:08:25 +0100

    feat: add telegram bot

--- bot/bot.js
+++ bot/bot.js
@@ -2,8 +2,6 @@ const { Telegraf } = require("telegraf");
 const { message } = require("telegraf/filters");
 
 const bot = new Telegraf("7538734070:AAHyqf1ejhzLwvIZe7QcLdRDRK4mNLoNuRg");
-bot.start((ctx) => ctx.reply("Welcome"));
-// bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
-// bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.start((ctx) => ctx.reply("t.me/fallingcoins_bot/flyingcoins"));
+
 bot.launch();
