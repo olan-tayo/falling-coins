ff9d737723d869160c2c0db85738b1b556a2d3ca
olan-tayo <olantayo3627@gmail.com>
2024-09-04 14:41:11 +0100

    feat: add telegram bot

--- bot/bot.js
+++ bot/bot.js
@@ -2,12 +2,7 @@ const { Telegraf } = require("telegraf");
 const bot = new Telegraf("7538734070:AAHyqf1ejhzLwvIZe7QcLdRDRK4mNLoNuRg");
 
 bot.start((ctx) => {
-  ctx.reply(
-    "Welcome! Click the button below:",
-    Markup.inlineKeyboard([
-      Markup.button.url("Launch", "https://falling-coins.vercel.app/"),
-    ])
-  );
+  ctx.reply("Welcome! Click the button below:");
 });
 
 bot.launch();
